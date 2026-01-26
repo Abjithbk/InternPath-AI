f8715a85a43ae9f87e48fc8f6b1eaad52225862c
Abjithbk <bkabjith2@gmail.com>
2026-01-26 11:00:12 +0530

    signup and login integrated in frontend also improved the backend folder structure..

--- intern-path-backend/database/database.py
+++ intern-path-backend/database/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 #env for render guyss
 DATABASE_URL = os.getenv("DB_URL")
 
--- intern-path-frontend/src/pages/LoginPage.jsx
+++ intern-path-frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,141 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+import api from "../axios";
+import { toast } from "react-toastify";
+import { GoogleLogin,GoogleOAuthProvider } from "@react-oauth/google"
 
 const LoginPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default LoginPage
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/login", formData);
+
+      // If backend returns token
+      if (res.data?.access_token) {
+        localStorage.setItem("access_token", res.data.access_token);
+      }
+
+      toast.success("Login successful 🎉");
+      navigate("/");
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+        error.response?.data?.detail ||
+        "Invalid email or password"
+      );
+    }
+  };
+
+  const handleGoogleLogin = async (response) => {
+    try {
+      const token = response.credential;
+      const res = await  api.post("/google-auth",{token});
+
+      localStorage.setItem("access_token",res.data.access_token);
+      toast.success("Google Login Success!");
+      navigate("/")
+    }
+    catch(err) {
+       toast.error("Google login failed!");
+
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="w-full max-w-xl px-6">
+        {/* Heading */}
+        <h1 className="text-3xl font-bold text-center text-slate-900 mb-10">
+          Welcome Back to InternPath
+        </h1>
+
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <GoogleLogin
+            onSuccess={handleGoogleLogin}
+            onError={() => toast.error("Google Login Failed")}
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="w-full flex items-center justify-center gap-3 border border-gray-300 rounded-full py-3 text-sm font-medium hover:bg-gray-50 transition"
+              >
+                <FcGoogle size={20} />
+                Continue with Google
+              </button>
+            )}
+          />
+        </GoogleOAuthProvider>
+
+
+        {/* OR Divider */}
+        <div className="flex items-center my-8">
+          <div className="flex-1 h-px bg-gray-300"></div>
+          <span className="px-4 text-gray-500 text-sm">OR</span>
+          <div className="flex-1 h-px bg-gray-300"></div>
+        </div>
+
+        {/* Form */}
+        <form onSubmit={handleLogin} className="space-y-6">
+          {/* Email */}
+          <div>
+            <label className="text-sm text-gray-600">Email</label>
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              type="email"
+              required
+              className="w-full mt-1 border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="text-sm text-gray-600">Password</label>
+            <input
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              type="password"
+              required
+              className="w-full mt-1 border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-3 rounded-lg font-semibold hover:bg-indigo-700 transition"
+          >
+            Login
+          </button>
+        </form>
+
+        {/* Footer */}
+        <p className="text-center text-sm text-gray-600 mt-6">
+          Don’t have an account?{" "}
+          <span
+            onClick={() => navigate("/Signup")}
+            className="text-indigo-600 font-medium cursor-pointer hover:underline"
+          >
+            Sign up
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
